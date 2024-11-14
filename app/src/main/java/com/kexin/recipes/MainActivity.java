063379a41957cd9de8e6b262ab1f121c0524ef55
Marina <marinacarvalho@protonmail.com>
2024-11-14 12:14:49 -0500

    impl favorites slider to filter recipes

--- app/src/main/java/com/kexin/recipes/MainActivity.java
+++ app/src/main/java/com/kexin/recipes/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
@@ -34,7 +36,21 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.rcv_recipes);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(recipeAdapter);
+
+        SwitchCompat switchFavorites = findViewById(R.id.switch_favorites);
+        FilterFavorites(switchFavorites);
+
         new LoadRecipesTask().execute();
+    }
+
+    private void FilterFavorites(SwitchCompat switchFavorites) {
+        switchFavorites.setOnCheckedChangeListener((CompoundButton buttonView, boolean isChecked) -> {
+            if (isChecked) {
+                new LoadFavoriteRecipesTask().execute();
+            } else {
+                new LoadRecipesTask().execute();
+            }
+        });
     }
 
     private class LoadRecipesTask extends AsyncTask<Void, Void, List<Recipe>> {
@@ -57,7 +73,29 @@ public class MainActivity extends AppCompatActivity {
             recyclerView.setAdapter(recipeAdapter);
         }
     }
+
+    private class LoadFavoriteRecipesTask extends AsyncTask<Void, Void, List<Recipe>> {
+        @Override
+        protected List<Recipe> doInBackground(Void... voids) {
+            AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                    AppDatabase.class, "recipes").build();
+            RecipeDAO recipeDAO = db.recipeDao();
+            return recipeDAO.getByFavorite();
+        }
+
+        @Override
+        protected void onPostExecute(List<Recipe> recipes) {
+            super.onPostExecute(recipes);
+            if (recipes == null) {
+                recipes = new ArrayList<>();
+            }
+            recipeAdapter = new RecipeAdapter(recipes);
+            recyclerView.setAdapter(recipeAdapter);
+        }
+    }
+
     public void createClicked(View view) {
         Intent intent = new Intent(this, DetailActivity.class);
         startActivity(intent);
     }
+}
